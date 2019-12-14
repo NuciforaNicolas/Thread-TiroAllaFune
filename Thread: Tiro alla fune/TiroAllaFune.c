e6bf603ff9360511513e98b9bf10360e7c22f8fc
Nicolas Nucifora <57483134+NicolasNucifora1997@users.noreply.github.com>
2019-12-14 15:38:36 +0100

    Add files via upload
    Improvement of TiroAllaFune (refactoring of the code)

--- Thread: Tiro alla fune/TiroAllaFune.c
+++ Thread: Tiro alla fune/TiroAllaFune.c
@@ -1,92 +1,87 @@
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
-#include <string.h>
-#include <pthread.h>
 
-pthread_t giocatori[2];
-pthread_mutex_t mut;
-int posizione;
-long vittorie_tp0, vittorie_tp1;
-pthread_cond_t wait_player0, wait_player1;
+pthread_t tp[2];
+pthread_mutex_t mutex;
+int pos = 0; 
+long vittorie_tp0 = 0, vittorie_tp1 = 0;
+pthread_cond_t wait_pt0, wait_pt1;
 
-void * play(void* ptr){
-    int recupero = 0;
-    int forza = 0;
+void *tp0_run(){
     while(1){
-        recupero = rand() % 4;
-        forza = rand() % 6;
+
+        if(vittorie_tp0 == 10 || vittorie_tp1 == 10)
+            pthread_exit((void *)vittorie_tp0);
+
+        int recupero = rand() % 4;
+        int forza = rand() % 6;
         sleep(recupero);
 
-        printf("\t\t\t\tPosizione: %d\n\n", posizione);
-
-        if(pthread_equal(pthread_self(), giocatori[0])){
-            pthread_mutex_lock(&mut);
-
-            if(posizione >= 10){
+        pthread_mutex_lock(&mutex);
+            if(pos >= 10){
                 vittorie_tp1++;
-                posizione = 0;
-                pthread_cond_signal(&wait_player1);
+                pos = 0;
+                printf("\t\t\ttp1 won: %ld\n", vittorie_tp1);
+                pthread_cond_signal(&wait_pt1);
             }
             
-            posizione -= forza;
-            printf("Forza giocatore[0]: %d\n", forza);
-            if(posizione <= -10){
-                pthread_cond_wait(&wait_player0, &mut);
-            }
+            pos -= forza;
+            printf("tp0 position: %d\n", pos);
+            if(pos <= -10)
+                pthread_cond_wait(&wait_pt0, &mutex);
+            
+        pthread_mutex_unlock(&mutex);
+    }
+}
 
-            printf("Vittorie tp[0]: %ld\n", vittorie_tp0);
-            pthread_mutex_unlock(&mut); 
-        }
-        else if(pthread_equal(pthread_self(), giocatori[0])){
-            pthread_mutex_lock(&mut);
+void *tp1_run(){
+    while(1){
+        if(vittorie_tp1 == 10 || vittorie_tp0 == 10)
+            pthread_exit((void *)vittorie_tp1);
 
-            if(posizione <= -10){
+        int recupero = rand() % 4;
+        int forza = rand() % 6;
+        sleep(recupero);
+
+        pthread_mutex_lock(&mutex);
+            if(pos <= -10){
                 vittorie_tp0++;
-                posizione = 0;
-                pthread_cond_signal(&wait_player0);
-            }
-            
-            posizione += forza;
-            printf("\t\t\t\t\t\t\tForza giocatore[1]: %d\n", forza);
-            if(posizione >= 10){
-                pthread_cond_wait(&wait_player1, &mut);  
+                pos = 0;
+                printf("tp0 won: %ld\n", vittorie_tp0);
+                pthread_cond_signal(&wait_pt0);
             }
 
-            printf("\t\t\t\t\t\t\tVittorie tp[1]: %ld\n", vittorie_tp1);
-            pthread_mutex_unlock(&mut); 
-        }
+            pos += forza;
+            printf("\t\t\ttp1 position: %d\n", pos);
+            if(pos >= 10)
+                pthread_cond_wait(&wait_pt1, &mutex);
 
-        if(vittorie_tp0 == 10)
-            pthread_exit((void *)vittorie_tp0);
-        else 
-            if(vittorie_tp1 == 10)
-                pthread_exit((void *) vittorie_tp1);
-
+        pthread_mutex_unlock(&mutex);
     }
 }
 
 int main(int argc, char** argv){
+    pthread_mutex_init(&mutex, NULL);
+    pthread_cond_init(&wait_pt0, NULL);
+    pthread_cond_init(&wait_pt1, NULL);
 
-    long res = 0;
-    posizione = 0;
-    vittorie_tp0 = 0;
-    vittorie_tp1 = 0;
+    pthread_create(&tp[0], NULL, &tp0_run, NULL);
+    pthread_create(&tp[1], NULL, &tp1_run, NULL);
 
-    pthread_mutex_init(&mut, NULL);
-    pthread_cond_init(&wait_player0, NULL);
-    pthread_cond_init(&wait_player1, NULL);
+    long restp0 = 0;
+    long restp1 = 0;
 
-    pthread_create(&giocatori[0], NULL, &play, NULL);
-    pthread_create(&giocatori[1], NULL, &play, NULL);
+    pthread_join(tp[1], (void *)&restp1);
+    pthread_join(tp[0], (void *)&restp0);
 
-    pthread_join(giocatori[1], (void *)&res);
-    pthread_join(giocatori[0], (void *)&res);
 
-    if(res == vittorie_tp0)
-        printf("\n\nPlayer 0 won! Score: %ld\n", res);
+    if(restp0 > restp1)
+        printf("tp0 reached more victories than tp1: %ld\n", restp0);
     else
-        printf("\n\nPlayer 1 won! Score: %ld\n", res);
+        printf("tp1 reached more victories than tp0: %ld\n", restp1);
 
-
+    return 0;
 }
